e16c15adbfcf8f9e9b6df11a24a11bbf4856ce6c
MSanjana041 <sanjanamadhavan2005@gmail.com>
2026-01-31 15:55:22 +0530

    add i18n translations

--- src/components/Farmerdashboard.jsx
+++ src/components/Farmerdashboard.jsx
@@ -1,10 +1,13 @@
 import CropListingForm from "./CropListingForm";
+import { useTranslation } from "react-i18next";
 
 
 function Farmerdashboard() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Farmer Dashboard</h1>
+      <h1>{t("dashboard.farmer_dashboard")}</h1>
       <CropListingForm/>
     </div>
   );
